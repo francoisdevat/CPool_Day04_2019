78edb213e1bce2698edb0648ba7b88bc904cca00
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-03 12:55:17 -0400

    add_of_new_my_putstr.c

--- my_putstr.c
+++ my_putstr.c
@@ -4,16 +4,15 @@
 ** File description:
 ** je comprend rien aux strings
 */
-void my_putchar(char c);
 
 int my_putstr(char const *str)
 {
     int i = 0;
 
-    while (str[i] != *str['\0']);
-           
+    while (str[i] != '\0')           
     {
         my_putchar(str[i]);
         ++i;
-    }        
+    }
+    return 0;
 }

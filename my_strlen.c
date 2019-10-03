19cb5ca161149549591dfaa2a863791834a14ef7
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-03 13:42:47 -0400

    add of my_strlen.c

---
+++ my_strlen.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** fonction qui compte le nombbre de caractere
+** File description:
+** c'est bon je commence à m'en sortir
+*/
+
+int my_strlen(char const *str)
+{
+    int i = 0;
+
+    if (!str)
+        return 1;
+    while(str[i] != '\0')
+    {
+        ++i;
+    }
+    return i;
+}

7ecae7261185186437251ca9f91b5963df55c4f7
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-03 10:22:55 -0400

    add_of_my_swap.c

---
+++ my_swap.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** alternance de deux adresse
+** File description:
+** fonction complexe des pointeurs
+*/
+
+int my_swap(int *a, int *b)
+{
+    int d;
+    
+    d = *a;
+    *a = *b;
+    *b = d;
+}        
+
+
